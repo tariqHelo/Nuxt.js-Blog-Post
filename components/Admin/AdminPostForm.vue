60ae66a999d1434721742b44e3f23db6f9db3d8d
unknown <tariq.1994t12@gmail.com>
2021-04-28 05:41:20 +0300

    Adds

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -1,12 +1,21 @@
 <template>
-<form @submit.prevent="onSave">
+  <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
-    <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.content">Content</AppControlInput>
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">Preview Text</AppControlInput>
     <AppButton type="submit">Save</AppButton>
-    <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
-</form>
+    <AppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel">Cancel</AppButton>
+  </form>
 </template>
 
 <script>
@@ -14,39 +23,38 @@ import AppControlInput from "@/components/UI/AppControlInput";
 import AppButton from "@/components/UI/AppButton";
 
 export default {
-    components: {
-        AppControlInput,
-        AppButton
-    },
-    props: {
-        post: {
-            type: Object,
-            required: false
-        }
-    },
-    data() {
-        return {
-            editedPost: this.post ?
-                {
-                    ...this.post
-                } :
-                {
-                    author: "",
-                    title: "",
-                    thumbnailLink: "",
-                    content: ""
-                }
-        };
-    },
-    methods: {
-        onSave() {
-            // Save the post
-            console.log(this.editedPost);
-        },
-        onCancel() {
-            // Navigate back
-            this.$router.push("/admin");
-        }
+  components: {
+    AppControlInput,
+    AppButton
+  },
+  props: {
+    post: {
+      type: Object,
+      required: false
     }
+  },
+  data() {
+    return {
+      editedPost: this.post
+        ? { ...this.post }
+        : {
+            author: "",
+            title: "",
+            thumbnail: "",
+            content: "",
+            previewText: ""
+          }
+    };
+  },
+  methods: {
+    onSave() {
+      // Save the post
+      this.$emit('submit', this.editedPost)
+    },
+    onCancel() {
+      // Navigate back
+      this.$router.push("/admin");
+    }
+  }
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
     return new Vuex.Store({
@@ -8,28 +9,56 @@ const createStore = () => {
         mutations: {
             setPosts(state, posts) {
                 state.loadedPosts = posts;
+            },
+            addPost(state, post) {
+                state.loadedPosts.push(post)
+            },
+            editPost(state, editedPost) {
+                const postIndex = state.loadedPosts.findIndex(
+                    post => post.id === editedPost.id
+                );
+                state.loadedPosts[postIndex] = editedPost
             }
         },
         actions: {
             nuxtServerInit(vuexContext, context) {
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        vuexContext.commit("setPosts", [{
-                                id: "1",
-                                title: "First Post",
-                                previewText: "This is our first post!",
-                                thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-                            },
-                            {
-                                id: "2",
-                                title: "Second Post",
-                                previewText: "This is our second post!",
-                                thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-                            }
-                        ]);
-                        resolve();
-                    }, 1000);
-                });
+                return axios
+                    .get("https://nuxt-blog.firebaseio.com/posts.json")
+                    .then(res => {
+                        const postsArray = [];
+                        for (const key in res.data) {
+                            postsArray.push({
+                                ...res.data[key],
+                                id: key
+                            });
+                        }
+                        vuexContext.commit("setPosts", postsArray);
+                    })
+                    .catch(e => context.error(e));
+            },
+            addPost(vuexContext, post) {
+                const createdPost = {
+                    ...post,
+                    updatedDate: new Date()
+                }
+                return axios
+                    .post("https://nuxt-blog.firebaseio.com/posts.json", createdPost)
+                    .then(result => {
+                        vuexContext.commit('addPost', {
+                            ...createdPost,
+                            id: result.data.name
+                        })
+                    })
+                    .catch(e => console.log(e));
+            },
+            editPost(vuexContext, editedPost) {
+                return axios.put("https://nuxt-blog.firebaseio.com/posts/" +
+                        editedPost.id +
+                        ".json", editedPost)
+                    .then(res => {
+                        vuexContext.commit('editPost', editedPost)
+                    })
+                    .catch(e => console.log(e))
             },
             setPosts(vuexContext, posts) {
                 vuexContext.commit("setPosts", posts);
