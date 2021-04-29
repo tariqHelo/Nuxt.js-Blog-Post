923ee048f3e4d627625a8b8985b095e36b4424dd
unknown <tariq.1994t12@gmail.com>
2021-04-30 01:44:10 +0300

    Backend

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,7 @@
   <div class="admin-page">
     <section class="new-post">
       <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
+      <AppButton style="margin-left: 10px" @click="onLogout">Logout</AppButton>
     </section>
     <section class="existing-posts">
       <h1>Existing Posts</h1>
@@ -13,21 +14,21 @@
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList'
-import AppButton from '@/components/UI/AppButton'
-
 export default {
-  layout: 'admin',
-  components: {
-    PostList,
-    AppButton
-  },
+  layout: "admin",
+  middleware: ["check-auth", "auth"],
   computed: {
     loadedPosts() {
-      return this.$store.getters.loadedPosts
+      return this.$store.getters.loadedPosts;
+    }
+  },
+  methods: {
+    onLogout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/admin/auth");
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -45,4 +46,3 @@ export default {
   text-align: center;
 }
 </style>
-
